63c3224a632bc1ce0b75af7e0a6ef70a8a96a459
eyurdakul <ejder.yurdakul@gmail.com>
2014-07-13 18:10:38 +0200

    added some more

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,14 +13,16 @@
           tasks: ["compass"]
         }
       },
-      coffee: {
-        expand: true,
-        flatten: true,
-        cwd: "" + __dirname + "/src/",
-        src: ["*.coffee"],
-        dest: "scripts/",
-        ext: ".js"
-      },
+      coffee: [
+        {
+          expand: true,
+          flatten: true,
+          cwd: "" + __dirname + "/src/",
+          src: ["*.coffee"],
+          dest: "scripts/",
+          ext: ".js"
+        }
+      ],
       compass: {
         dist: {
           options: {
@@ -28,12 +30,31 @@
             cssDir: "" + __dirname + "/styles/"
           }
         }
+      },
+      bowercopy: {
+        options: {
+          srcPrefix: "bower_components"
+        },
+        script: {
+          options: {
+            destPrefix: "scripts/vendor"
+          }
+        }
+      },
+      connect: {
+        options: {
+          port: 8080,
+          open: true,
+          hostname: "localhost"
+        }
       }
     });
     grunt.loadNpmTasks("grunt-contrib-coffee");
-    grunt.loadNpmTasks("grunt-contrib-sass");
+    grunt.loadNpmTasks("grunt-contrib-compass");
     grunt.loadNpmTasks("grunt-contrib-watch");
-    return grunt.registerTask("default", ["coffee", "compass", "watch"]);
+    grunt.loadNpmTasks("grunt-contrib-connect");
+    grunt.loadNpmTasks("grunt-bowercopy");
+    grunt.registerTask("default", ["coffee", "compass", "bowercopy", "watch", "connect"]);
   };
 
 }).call(this);
--- src/Gruntfile.coffee
+++ src/Gruntfile.coffee
@@ -9,21 +9,37 @@ module.exports = (grunt)->
       compass:
         files: "src/styles/*.scss"
         tasks: ["compass"]
-    coffee:
-      expand: true
-      flatten: true
-      cwd: "#{__dirname}/src/"
-      src: ["*.coffee"]
-      dest: "scripts/"
-      ext: ".js"
+    coffee: [
+              (
+               expand: true
+               flatten: true
+               cwd: "#{__dirname}/src/"
+               src: ["*.coffee"]
+               dest: "scripts/"
+               ext: ".js"
+              )
+            ]
     compass:
       dist:
         options:
           sassDir: "#{__dirname}/src/styles/"
           cssDir: "#{__dirname}/styles/"
+    bowercopy:
+      options:
+        srcPrefix: "bower_components"
+      script:
+        options:
+          destPrefix: "scripts/vendor"
+    connect:
+      options:
+        port: 8080
+        open: true
+        hostname: "localhost"
 
   grunt.loadNpmTasks "grunt-contrib-coffee"
-  grunt.loadNpmTasks "grunt-contrib-sass"
+  grunt.loadNpmTasks "grunt-contrib-compass"
   grunt.loadNpmTasks "grunt-contrib-watch"
-  grunt.registerTask "default", ["coffee", "compass", "watch"]
+  grunt.loadNpmTasks "grunt-contrib-connect"
+  grunt.loadNpmTasks "grunt-bowercopy"
+  grunt.registerTask "default", ["coffee", "compass", "bowercopy", "watch", "connect"]
   return
